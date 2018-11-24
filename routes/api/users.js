b30065ac313baf7adc1155b65fe70f54868f6f00
Andrew MacDonald <mac087@gmail.com>
2018-11-24 10:32:30 -0800

    Implemented user login logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,16 +9,20 @@ const User = require('../../models/User');
 // @route  GET api/users/test
 // @desc   Tests users route
 // @access Public
-router.get('/test', (req,res) => res.json({msg: 'Users Works' }));
+router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
 // @route  GET api/users/register
 // @desc   Register user
 // @access Public
 router.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then( user => {
-      if(user) {
-        return res.status(400).json({ email: 'Email already exists' });
+  User.findOne({
+      email: req.body.email
+    })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({
+          email: 'Email already exists'
+        });
       } else {
         const avatar = gravatar.url(req.body.email, {
           s: '200', // Size
@@ -35,7 +39,7 @@ router.post('/register', (req, res) => {
 
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) throw err;
+            if (err) throw err;
             newUser.password = hash;
             newUser.save()
               .then(user => res.json(user))
@@ -44,6 +48,34 @@ router.post('/register', (req, res) => {
         });
       }
     })
+});
+
+
+// @route  GET api/users/login
+// @desc   Login User / Returning JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if (isMatch) {
+          res.json({ msg: 'Success' });
+        } else {
+          return res.status(400).json({ password: 'Password incorrect' });
+        }
+      })
+  })
+
 });
 
 module.exports = router;
